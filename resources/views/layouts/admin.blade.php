22b5f44890c19d954c8aaa30dde5765e45e12b16
defaaryawar <defadefa1313@gmail.com>
2026-04-27 10:38:21 +0700

    fixing logo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -436,7 +436,7 @@
     {{-- Brand --}}
     <a href="{{ route('dashboard') }}" class="sidebar-brand">
         <div class="brand-icon">
-            <img src="{{ asset('img/LOGO.png') }}" alt="Logo SIPNONA">
+            <img src="{{ asset('img/LOGO.PNG') }}" alt="Logo SIPNONA">
         </div>
         <div class="brand-text">
             <h1>SIPNONA</h1>
